07155b3b8e5cd410e4b581d87125d3b810b9a96e
uktamyb <67410434+uktamyb@users.noreply.github.com>
2021-09-05 18:12:52 +0500

    Changes Components changes in 05.09.2021

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Information from "./Information";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Information detailInfo={props.tel} />
+        <Information detailInfo={props.email} />
       </div>
     </div>
   );
---
+++ src/Components/Information.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Information(props) {
+  return <p className="info">{props.detailInfo}</p>;
+}
+
+export default Information;
